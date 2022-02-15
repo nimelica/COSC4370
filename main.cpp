70ff0afc40702be0d9d0324191a176700c2d20ac
Nimet Ozkan <76903207+nimelica@users.noreply.github.com>
2022-02-15 03:48:18 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include "BMP.h"
+#include <cmath>
+/*NIMET OZKAN*/
+
+void DrawPoints(BMP& obj, long x_rad, long y_rad, long x_central, long y_central){
+  obj.set_pixel(x_rad+x_central, y_rad+y_central, 255, 255, 255, 0);
+  obj.set_pixel(-x_rad+x_central, y_rad+y_central, 255, 255, 255, 0);
+  obj.set_pixel(x_rad+x_central, -y_rad+y_central, 255, 255, 255, 0);
+  obj.set_pixel(-x_rad+x_central, -y_rad+y_central, 255, 255, 255, 0);
+}
+
+void DrawEllipse(BMP& newObj, long x_central, long y_central, long rad_x, long rad_y){
+  long stop_x, stop_y, quad1, quad2, x, y;
+  x = 0, y = rad_y;
+
+    quad1 = (rad_y * rad_y) - (rad_x * rad_x * rad_y) + (0.25 * rad_x * rad_x);
+    stop_x = 2 * rad_y * rad_y * x;
+    stop_y = 2 * rad_x * rad_x * y;
+
+    while(stop_x < stop_y){
+        DrawPoints(newObj, x, y, x_central, y_central);
+        if (quad1 < 0){
+            ++x;
+            stop_x = stop_x + (2 * rad_y * rad_y);
+            quad1 = quad1 + stop_x + (rad_y * rad_y);
+        }else{
+            ++x;
+            --y;
+            stop_x = stop_x + (2 * rad_y * rad_y);
+            stop_y = stop_y - (2 * rad_x * rad_x);
+            quad1 = quad1 + stop_x - stop_y + (rad_y * rad_y);
+        }
+    }
+   quad2 = ((rad_y * rad_y) * ((x + 0.5) * (x + 0.5))) +
+          ((rad_x * rad_x) * ((y - 1) * (y - 1))) -(rad_x * rad_x * rad_y * rad_y);
+  
+    while(y){
+        DrawPoints(newObj, x, y, x_central, y_central);
+        
+        if (quad2 > 0){
+            y--;
+            stop_y = stop_y - (2 * rad_x * rad_x);
+            quad2 = quad2 + (rad_x * rad_x) - stop_y;
+        }
+        else{
+            --y;
+            ++x;
+            stop_x = stop_x + (2 * rad_y * rad_y);
+            stop_y = stop_y - (2 * rad_x * rad_x);
+            quad2 = quad2 + stop_x - stop_y + (rad_x * rad_x);
+        }
+    }
+}
+
+int main(){
+  BMP newObj(1792, 1792, false);
+
+  long x_central = 896, y_central = 1400; //centers y >= 1
+  long rad_x = 768, rad_y = 384; //radius vals
+  
+  DrawEllipse(newObj, x_central, y_central, rad_x, rad_y);
+
+  newObj.write("output.bmp");
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
