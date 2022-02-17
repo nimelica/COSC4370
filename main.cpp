82368c8f6e68e579e3c40123148870f21c4057f1
Nimet Ozkan <76903207+nimelica@users.noreply.github.com>
2022-02-16 18:07:27 -0600

    updated main.cpp
    added cpp math functionality

--- main.cpp
+++ main.cpp
@@ -14,11 +14,11 @@ void DrawEllipse(BMP& newObj, long x_central, long y_central, long rad_x, long r
   long stop_x, stop_y, quad1, quad2, x, y;
   x = 0, y = rad_y;
 
-    quad1 = (rad_y * rad_y) - (rad_x * rad_x * rad_y) + (0.25 * rad_x * rad_x);
-    stop_x = 2 * rad_y * rad_y * x;
-    stop_y = 2 * rad_x * rad_x * y;
+    quad1 = pow(rad_y,2) - (pow(rad_x,2)*rad_y) + (0.25 * pow(rad_x,2));
+    stop_x = 2 * pow(rad_y,2) * x;
+    stop_y = 2 * pow(rad_x,2) * y;
 
-    while(stop_x < stop_y){
+     while(stop_x < stop_y){
         DrawPoints(newObj, x, y, x_central, y_central);
         if (quad1 < 0){
             ++x;
@@ -32,8 +32,8 @@ void DrawEllipse(BMP& newObj, long x_central, long y_central, long rad_x, long r
             quad1 = quad1 + stop_x - stop_y + (rad_y * rad_y);
         }
     }
-   quad2 = ((rad_y * rad_y) * ((x + 0.5) * (x + 0.5))) +
-          ((rad_x * rad_x) * ((y - 1) * (y - 1))) -(rad_x * rad_x * rad_y * rad_y);
+    quad2 = (pow(rad_y,2) * ((x + 0.5) * (x + 0.5))) +
+          (pow(rad_x,2) * ((y - 1) * (y - 1))) -(rad_x * rad_x * rad_y * rad_y);
   
     while(y){
         DrawPoints(newObj, x, y, x_central, y_central);
@@ -59,38 +59,13 @@ int main(){
   long x_central = 896, y_central = 1400; //centers y >= 1
   long rad_x = 768, rad_y = 384; //radius vals
   
+  // to see the ellipse in the middle of the screen:
+  // x_cetral = 800, y_central = 800 and window size (1600, 1600)
+  // but since y >= 1, put the ellipse about above the 0
+  
   DrawEllipse(newObj, x_central, y_central, rad_x, rad_y);
 
   newObj.write("output.bmp");
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
